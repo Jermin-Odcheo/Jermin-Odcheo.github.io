ce8df8ad6f7bedc109969e7bc49e7783c03d9527
JERMIN <2224981@slu.edu.ph>
2025-06-13 01:03:51 +0800

    Add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.html', './dist/**/*.html'],
   theme: {
-    extend: {},
+    
+    extend: {
+
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'spin-fast': 'spin .5s linear infinite',
+      }
+      
+    }
   },
   plugins: [],
 } 
-// tailwind.config.js
-module.exports = {
-    theme: {
-      extend: {
-        animation: {
-          'spin-slow': 'spin 3s linear infinite',
-          'spin-fast': 'spin .5s linear infinite',
-        }
-      }
-    }
-  }
-  
