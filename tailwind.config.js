4c28fe2915576fe2ed52baf2d63423c141d80563
JERMIN <2224981@slu.edu.ph>
2025-06-14 01:30:18 +0800

    initial about me

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html', './dist/**/*.html'],
-  theme: {
-    
-    extend: {
-
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-        'spin-fast': 'spin .5s linear infinite',
-      }
-      
-    }
-  },
-  plugins: [],
-} 
+    content: ['./*.html', './dist/**/*.html', "./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue",],
+    theme: {
+        extend: {
+            width: {
+                '7/10': '70%',
+            },
+            animation: {
+                'spin-slow': 'spin 3s linear infinite',
+                'spin-fast': 'spin .5s linear infinite',
+            },
+            // No need to explicitly define colors if using Tailwind's defaults
+        }
+    },
+    plugins: [],
+    // Safelist specific color classes to ensure they're included
+    safelist: [
+        'text-green-400',
+        'text-blue-400'
+    ]
+}
